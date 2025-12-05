0154cb8c8bbe210cf80305100a73e7e5f89f0aa2
Will Nobles <wnobles39@gmail.com>
2025-12-05 13:39:45 -0500

    Consolidate functions, add and rename variables

--- 2025/day1.py
+++ 2025/day1.py
@@ -1,52 +1,49 @@
-def get_zero_landings(data: list) -> int:
-    total = 0
-    position = 50
-    for line in data:
-        position = (position + line) % 100 # positions 0 to 99
-        if position == 0:
-            total += 1
-    return total
-
-def get_zero_crossings(data: list) -> int:
-    total = 0
+def get_zero_crossings(data: list) -> tuple[int, int]:
+    total_landings = 0
+    total_crossings = 0
     position = 50
 
     for line in data:
-        distance = position + line
-        next_pos = distance % 100
+        dist = abs(line)
+        next_pos = position + line
+        next_pos_cycled = next_pos % 100
+
+        # Part 1
+        if next_pos_cycled == 0:
+            total_landings += 1
 
         # Case 1: both zero positions
-        if position == 0 and next_pos == 0:
-            rotation = abs(line) // 100
-            total += rotation
+        if position == 0 and next_pos_cycled == 0:
+            rotation = dist // 100
+            total_crossings += rotation
 
         # Case 2: non-zero position, zero next position
-        elif position != 0 and next_pos == 0:
-            rotation = abs(line) // 100 + 1 # add one for landing on zero
-            total += rotation
+        elif position != 0 and next_pos_cycled == 0:
+            rotation = dist // 100 + 1 # add one for landing on zero
+            total_crossings += rotation
 
         # Case 3: zero position, non-zero next position
-        elif position == 0 and next_pos != 0:
-            rotation = abs(line) // 100
-            total += rotation
+        elif position == 0 and next_pos_cycled != 0:
+            rotation = dist // 100
+            total_crossings += rotation
 
         # Case 4: both non-zero positions
         else:
-            rotation = abs(line) // 100
-            left_over = (abs(line) % 100 if line > 0 else (abs(line) % 100) * -1)
-            extra = ((position + left_over) < 0) or ((position + left_over) > 100)
-            if (distance < 0 or distance > 100) and (abs(line) < 100):
-                total += 1
-            elif abs(line) > 100:
-                total += rotation + extra
+            rotation = dist // 100
+            left_over = (dist % 100 if line > 0 else (dist % 100) * -1)
+            remaining_dist = position + left_over
+            extra = remaining_dist < 0 or remaining_dist > 100
+            if (next_pos < 0 or next_pos > 100) and dist < 100:
+                total_crossings += 1
+            elif dist > 100:
+                total_crossings += rotation + extra
 
-        position = next_pos
+        position = next_pos_cycled
 
-    return total
+    return total_landings, total_crossings
 
 with open("2025/input.txt") as f:
     read_data = f.read().splitlines()
 read_data = [int(x.replace("L", "-").replace("R", "")) for x in read_data]
 
-print(f"Part 1 password: {get_zero_landings(read_data)}")
-print(f"Part 2 password: {get_zero_crossings(read_data)}")
+print(f"Passwords (Part 1, Part 2): {get_zero_crossings(read_data)}")
