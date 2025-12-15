4e2e6e0ac7cc4c6eacf88aa19559fdc7d724b519
Will Nobles <wnobles39@gmail.com>
2025-12-15 12:19:05 -0500

    Implement Part 2

--- 2025/day3.py
+++ 2025/day3.py
@@ -17,6 +17,37 @@ def part1(data: list) -> int:
 
     return num
 
+def part2(data: list) -> int:
+
+    num = 0
+    for line in data:
+
+        digits = ""
+        counter = 11
+        start = 0
+        while counter >= 0:
+
+            # Trim the list to be eleven, ten, nine, ..., zero away from the end
+            end = len(line) - counter
+            away_from_end = line[start:end]
+
+            # Get the max value and index in the sub-string
+            max_digit = str(max(map(int, away_from_end)))
+            max_idx = away_from_end.index(max_digit)
+
+            # Add the value to the string
+            digits += max_digit
+
+            # Increment the start and decrement the counter
+            start += max_idx + 1
+            counter -= 1
+
+        num += int(digits)
+
+    return num
+
+
 with open("2025/data/day3_input.txt") as f:
     read_data = f.read().splitlines()
-print(part1(read_data))
+print(f"Part1: {part1(read_data)}")
+print(f"Part2: {part2(read_data)}")
