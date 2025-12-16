81b1b871020709cdfb5e445d288b6bffaee70e6b
Will Nobles <wnobles39@gmail.com>
2025-12-16 10:14:55 -0500

    Consolidate functions, adjust while condition

--- 2025/day4.py
+++ 2025/day4.py
@@ -1,10 +1,11 @@
-def part1(data: list) -> int:
+def get_removed_rolls(data: list, remove_once: bool = True) -> int:
 
     num_rolls = 0
 
     coords_list = [
         (i, j) for i, line in enumerate(data) for j, x in enumerate(line)
         if x == "@"]
+    coords_to_remove = []
 
     for i, (a, b) in enumerate(coords_list):
 
@@ -16,20 +17,17 @@ def part1(data: list) -> int:
 
         if adjacent_count < 4:
             num_rolls += 1
+            coords_to_remove.append((a, b))
 
-    return num_rolls
+    if remove_once:
+        return num_rolls
 
-def part2(data: list) -> int:
+    while coords_to_remove:
 
-    num_rolls = 0
-
-    coords_list = [
-        (i, j) for i, line in enumerate(data) for j, x in enumerate(line)
-        if x == "@"]
-
-    while 1:
-
+        coords_list = [
+            item for item in coords_list if item not in coords_to_remove]
         coords_to_remove = []
+
         for i, (a, b) in enumerate(coords_list):
 
             search_list = coords_list[:i] + coords_list[i+1:]
@@ -43,16 +41,10 @@ def part2(data: list) -> int:
                 num_rolls += 1
                 coords_to_remove.append((a, b))
 
-        if coords_to_remove:
-            coords_list = [
-                item for item in coords_list if item not in coords_to_remove]
-        else:
-            break
-
     return num_rolls
 
 with open("2025/data/day4_input.txt") as f:
     read_data = f.read().splitlines()
 
-print(f"Part 1: {part1(read_data)}")
-print(f"Part 2: {part2(read_data)}")
+print(f"Part 1: {get_removed_rolls(read_data, remove_once=True)}")
+print(f"Part 2: {get_removed_rolls(read_data, remove_once=False)}")
