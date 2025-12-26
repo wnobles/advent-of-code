de3b743c9d444caf4a0f6d827f0c79350703b10a
Will Nobles <wnobles39@gmail.com>
2025-12-26 15:10:00 -0600

    Consolidate functions, refine file processing

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,34 +1,36 @@
-def part1(numbers: list, operators: list) -> int:
+def calculate_problems_sum(file_path: str, right_to_left: bool = False) -> int:
 
-    result = 0
+    with open(file_path) as f:
+        read_data = [line.rstrip("\n") for line in f]
 
-    num_problems = len(numbers[0])
-    for i in range(num_problems):
-        problem_numbers = [row[i] for row in numbers]
-        operator = operators[i]
-        if operator == "*":
-            product = 1
-            for num in problem_numbers:
-                product *= num
-            result += product
+    operators = read_data.pop(-1)
+
+    # Get the beginning and ending indices of each "column" of digits
+    problem_ranges = []
+    operator_start = 0
+    for i, c in enumerate(operators[1:], 1):
+        if c in ["*", "+"]:
+            problem_ranges.append([operator_start, i - 1]) # i-1 to ignore space
+            operator_start = i
+    problem_ranges.append([operator_start, i + 1]) # i+1 for exclusive end
+
+    # Collect the digits in the columns vertically right to left or row by row
+    problems = []
+    for start, stop in problem_ranges:
+        if right_to_left:
+            problem = []
+            for i in range(start, stop):
+                num = "".join(line[i] for line in read_data).strip()
+                problem.append(num)
         else:
-            result += sum(problem_numbers)
+            problem = [line[start:stop].strip() for line in read_data]
+        problems.append(problem)
 
-    return result
-
-with open("2025/data/day6_input.txt") as f:
-    numbers = []
-    for line in f:
-        if "*" not in line and "+" not in line:
-            number_row = list(map(int, line.split()))
-            numbers.append(number_row)
-    operators = line.split()
-print(f"Part 1: {part1(numbers, operators)}")
-
-def part2(problems: list, operators: list) -> int:
+    operators = operators.split()
 
     result = 0
 
+    # Do the math
     for i, operator in enumerate(operators):
         if operator == "*":
             product = 1
@@ -40,27 +42,6 @@ def part2(problems: list, operators: list) -> int:
 
     return result
 
-with open("2025/data/day6_input.txt") as f:
-    read_data = [line.rstrip("\n") for line in f]
-
-operators = read_data.pop(-1)
-
-problem_ranges = []
-operator_start = 0
-for i, c in enumerate(operators[1:], 1):
-    if c in ["*", "+"]:
-        problem_ranges.append([operator_start, i - 1])
-        operator_start = i
-problem_ranges.append([operator_start, i + 1])
-
-problems = []
-for start, stop in problem_ranges:
-    problem = []
-    for i in range(start, stop):
-        num = "".join(line[i] for line in read_data).strip()
-        problem.append(num)
-    problems.append(problem)
-
-operators = operators.split()
-
-print(f"Part 2: {part2(problems, operators)}")
+file_path = "2025/data/day6_input.txt"
+print(f"Part 1: {calculate_problems_sum(file_path)}")
+print(f"Part 2: {calculate_problems_sum(file_path, True)}")
